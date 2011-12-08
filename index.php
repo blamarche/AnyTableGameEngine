834e35769e6fdcde20dd11ab259c55835668d0cd
dracflamloc <drac@dracsoft.com>
2011-12-08 11:21:16 -0500

    base

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php 
+/*
+	Data for game:
+
+	Background/Board
+	Addable Pieces
+		ImageURL
+		Rotation
+		User-addable?
+		User-removable?
+		Position
+		Z-index
+	Default Pieces
+		ImageURL
+		Rotation
+		User-removable?
+		Position
+		Z-index
+	Notes
+	Chat
+	Rolls
+	
+*/
+
+
+?>
